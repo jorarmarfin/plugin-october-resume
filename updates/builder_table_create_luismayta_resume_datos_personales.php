37d4ae53314b6e791cbc727b5f385ad182bd66bc
Luis mayta <luis.mayta@gmail.com>
2019-07-22 17:50:22 -0500

    First commit

---
+++ updates/builder_table_create_luismayta_resume_datos_personales.php
@@ -0,0 +1,25 @@
+<?php namespace LuisMayta\Resume\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateLuismaytaResumeDatosPersonales extends Migration
+{
+    public function up()
+    {
+        Schema::create('luismayta_resume_datos_personales', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('nombre_completo');
+            $table->string('nombre_corto');
+            $table->string('fecha_nacimiento');
+            $table->string('estado_civil');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('luismayta_resume_datos_personales');
+    }
+}
